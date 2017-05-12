cff6874a6618fd826f9e7576ea52256063fb64b6
Matt Snively <msnively@apperian.com>
2017-05-12 13:50:30 +0000

    Remove apt-get installs for utilities not needed on the server

--- INSTALL.sh
+++ INSTALL.sh
@@ -250,8 +250,9 @@ mac_setup() {
 }
 
 linux_setup() {
-    apt-get install ideviceinstaller
-    apt-get install libimobiledevice-utils
+    #apt-get install ideviceinstaller
+    #apt-get install libimobiledevice-utils
+    echo "Skipping apt-get installs, we use yum"
 }
 
 
