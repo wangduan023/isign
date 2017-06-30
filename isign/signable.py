e0a9b7c81691162734dbc17205fca64f584d94fe
Matt Snively <msnively@apperian.com>
2017-06-30 18:14:41 +0000

    Change usage of os.rename to shutil.move
    Under certain circumstances, using os.rename can raise an OSError. From the Python docs:
    "The operation may fail on some Unix flavors if src and dst are on different filesystems."

--- isign/signable.py
+++ isign/signable.py
@@ -18,6 +18,7 @@ from makesig import make_signature
 import os
 import tempfile
 import utils
+import shutil
 
 log = logging.getLogger(__name__)
 
@@ -254,7 +255,7 @@ class Signable(object):
         mode = os.stat(self.path).st_mode
         os.chmod(temp.name, mode)
         # log.debug("moving temporary file to {0}".format(self.path))
-        os.rename(temp.name, self.path)
+        shutil.move(temp.name, self.path)
 
 
 class Executable(Signable):
